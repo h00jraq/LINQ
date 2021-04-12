494e416aba2a2feaa6f28afd7eb586c31b5cf2b8
h00jraq <filip.drzewiecki@yahoo.com>
2021-04-12 23:30:40 +0200

    Added a lot of stuff

---
+++ LINQ_extension_methods/Queries/MyLinq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    public static class MyLinq
+    {
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> sourceData,
+                                               Func<T, bool> predicate)
+        {
+
+            foreach (var item in sourceData)
+            {
+                if (predicate(item))
+                    yield return item;
+            }
+
+        }
+    }
+}
---
+++ LINQ_extension_methods/Queries/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var movies = new List<Movie>
+            { 
+                new Movie { Title = "Godzilla", Rating = 8.5f, Year = 2015 },
+                new Movie { Title = "King-Kong", Rating = 9.0f, Year = 2012 },
+                new Movie { Title = "LOTR", Rating = 10.0f, Year = 2005 },
+                new Movie { Title = "Batman", Rating = 7.25f, Year = 2010 },
+                new Movie { Title = "Spiderman", Rating = 7.25f, Year = 1988 },
+                new Movie { Title = "Star Wars VI", Rating = 7.25f, Year = 1995 }
+            };
+            Console.WriteLine("objects created");
+            var query = movies.Where(m => m.Year > 2000);
+                              
+
+            foreach (var movie in query)
+            {
+                Console.WriteLine(movie.Title);
+            }
+            Console.WriteLine("---Extensiion Linq method---");
+
+            var query3 = movies.Filter(m => m.Year > 2000);
+                             
+
+            foreach (var movie in query3)
+            {
+                Console.WriteLine(movie.Title);
+            }
+            Console.WriteLine("--My Own Extension method---");
+
+            var query2 = from movie in movies
+                         where movie.Year > 2000
+                         select movie;
+
+            foreach (var movie in query2)
+            {
+                Console.WriteLine(movie.Title);
+            }
+            Console.WriteLine("--Linq query---");
+        }
+    }
+}
